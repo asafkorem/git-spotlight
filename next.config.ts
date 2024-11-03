29e653d72de6d7a37c3292b23d1a5927826090e9
Asaf Korem <asaf.korem@gmail.com>
2024-11-03 21:32:29 +0200

    fix ci

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: process.env.NODE_ENV === 'production' ? '/git-spotlight' : '',
     images: {
         unoptimized: true,
     },
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/git-spotlight' : '',
 }
 
 module.exports = nextConfig
